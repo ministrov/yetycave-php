6bbdd1c9933da94571abdff2b03b0cea06199aa8
unknown <ministrov2018@gmail.com>
2024-01-04 20:07:25 +0300

    feat: end up operating with module1-task1

--- index.php
+++ index.php
@@ -3,6 +3,15 @@ $is_auth = rand(0, 1);
 
 $user_name = 'Anton'; // укажите здесь ваше имя
 
+$categories = [
+    [],
+    [],
+    [],
+    [],
+    [],
+    []
+];
+
 ?>
 <!DOCTYPE html>
 <html lang="ru">
