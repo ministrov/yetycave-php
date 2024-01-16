88178e3bbf88a6705272313fcba2f5b77b13a4ef
unknown <ministrov2018@gmail.com>
2024-01-16 13:29:52 +0300

    feat: start operating on module5

--- queries.sql
+++ queries.sql
@@ -1,16 +1,18 @@
-INSERT INTO categories (character_code, name_category) 
-VALUES 
-  ('boards', 'Доски и лыжи'),
-  ('bindings', 'Крепления'),
-  ('boots', 'Ботинки'),
-  ('cloths', 'Одежда'),
-  ('tools', 'Инструменты'),
-  ('other', 'Разное')
-
-INSERT INTO users (email, user_name, user_password, contacts)
+INSERT INTO categories 
+  (character_code, name_category)
 VALUES
-  (`hero34@mail.ru`, 'Ярослав', 'secretpassw1', '89191202527'),
-  (`asis174@mail.ru`, 'Слава', 'secretpassw2', '83512254836')
+	('boards', 'Доски и лыжи'),
+  ('attachment', 'Крепления'),
+	('boots', 'Ботинки'),
+  ('clothing', 'Одежда'),
+  ('tools', 'Инструменты'),
+  ('other', 'Разное');
+
+INSERT INTO users 
+  (email, user_name, user_password, contacts)
+VALUES
+	('hero34@mail.ru', 'Ярослав', 'secretpassw1', '89191202527'),
+  ('asis174@mail.ru', 'Слава', 'secretpassw2', '83512254836');
 
 INSERT INTO lots
 	(title, lot_description, img, start_price, date_finish, step, user_id, category_id)
@@ -20,7 +22,8 @@ VALUES
   ('Крепления Union Contact Pro 2015 года размер L/XL', 'Хорошие крепления, надежные и легкие', 'img/lot-3.jpg', 8000, '2021-08-12', 500, 2, 2),
   ('Ботинки для сноуборда DC Mutiny Charocal', 'Теплые и красивые ботинки', 'img/lot-4.jpg', 10999, '2021-08-13', 600, 1, 3),
   ('Куртка для сноуборда DC Mutiny Charocal', 'Легкая, теплая и прочная куртка', 'img/lot-5.jpg', 7500, '2021-08-14', 500, 1, 4),
-  ('Маска Oakley Canopy', 'Желтые очки, все будет веселенькое', 'img/lot-6.jpg', 5400, '2021-08-15', 100, 1, 6)
+  ('Маска Oakley Canopy', 'Желтые очки, все будет веселенькое', 'img/lot-6.jpg', 5400, '2021-08-15', 100, 1, 6);
+
 
 INSERT INTO bets
 	(price_bet, user_id, lot_id)
