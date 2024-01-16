1b70f1d5692d6c1ba105fd01296ade5bd32ac118
unknown <ministrov2018@gmail.com>
2024-01-16 15:55:44 +0300

    feat: try to connect to MySQL from PHP

--- testing.php
+++ testing.php
@@ -1,11 +1,28 @@
 <?php
 
-// $a = 1;
-$number_1 = 4;
-$number_2 = 3;
+$con = mysqli_connect("localhost", "root", "", "yeticave");
+$sql = "SELECT * FROM categories";
+$result = mysqli_query($con, $sql);
 
-$b = "Hello World";
-$name = 'Anton';
+$result_row = mysqli_fetch_assoc($result);
+
+// $result_count = mysqli_num_rows($result);
+
+// print(json_encode($result_row));
+print($result_row);
+
+// $categories = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+// foreach ($categories as $category) {
+//   print("Category: " . $category);
+// }
+
+// $a = 1;
+// $number_1 = 4;
+// $number_2 = 3;
+
+// $b = "Hello World";
+// $name = 'Anton';
 // $brother = 'Denis';
 // const PI = 3.14;
 // $result = $name . ' ' . $brother;
@@ -27,18 +44,34 @@ $name = 'Anton';
 // echo $result;
 // echo PI;
 
-$new_str = str_replace("World", "bg", $b);
+// $new_str = str_replace("World", "bg", $b);
 
-echo $new_str;
+// echo $new_str;
 
-function calculate_numbers($num_1, $num_2) {
-  return $num_1 + $num_2;
-}
+// function calculate_numbers($num_1, $num_2) {
+//   return $num_1 + $num_2;
+// }
 
-echo calculate_numbers($number_1, $number_2);
+// echo calculate_numbers($number_1, $number_2);
 
-function personal_info($name, $age) {
-  echo "My name is $name, and I'm $age";
-}
+// function personal_info($name, $age) {
+//   echo "My name is $name, and I'm $age";
+// }
 
-personal_info($name, 40);
+// personal_info($name, 40);
+
+// $con = mysqli_connect("localhost", "root", "", "yeticave");
+
+// mysqli_set_charset($con, "utf8");
+
+// $categories = mysqli_query($con, "SELECT * FROM categories");
+
+// while ($row = $categories -> fetch_assoc()) {
+//   print($row);
+// }
+
+// if ($con == false) {
+//   print("Error of connection: " . mysqli_connect_error());
+// } else {
+//   print($categories);
+// }
