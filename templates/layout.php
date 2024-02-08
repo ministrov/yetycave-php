4de455691a9233308984653900e01c40cb7b712c
unknown <ministrov2018@gmail.com>
2024-02-08 13:03:03 +0300

    feat: start operating on creating search page, add search template

--- templates/layout.php
+++ templates/layout.php
@@ -17,11 +17,11 @@
         <a class="main-header__logo">
           <img src="../img/logo.svg" width="160" height="39" alt="Логотип компании YetiCave">
         </a>
-        <form class="main-header__search" method="get" action="https://echo.htmlacademy.ru" autocomplete="off">
-          <input type="search" name="search" placeholder="Поиск лота">
+        <form class="main-header__search" method="get" action="search.php" autocomplete="off">
+          <input type="search" name="search" placeholder="Поиск лота" value="<?= $search; ?>">
           <input class="main-header__search-btn" type="submit" name="find" value="Найти">
         </form>
-        <a class="main-header__add-lot button" href="pages/add-lot.html">Добавить лот</a>
+        <a class="main-header__add-lot button" href="add.php">Добавить лот</a>
 
         <nav class="user-menu">
           <!-- здесь должен быть PHP код для показа меню и данных пользователя -->
---
+++ templates/main-search.php
@@ -0,0 +1,7 @@
+<main>
+  <div class="container">
+    <section class="lots">
+      <h2>Результаты поиска по запросу «<span><?= $search; ?></span>»</h2>
+    </section>
+  </div>
+</main>
